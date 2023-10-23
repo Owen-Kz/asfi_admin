13259d3e61209aaae7152c030a1be7cb894f03a0
Owen-Kz <lordkz19@gmail.com>
2023-10-23 23:42:07 +0100

    Update pages.js

--- routes/pages.js
+++ routes/pages.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.use(express.json())
 
 router.get("/", (req,res)=>{
-    res.render("admin/dashboard")
+    res.render("admin-dashboard")
 })
 
 router.get("/admin/courses", (req,res) =>{
